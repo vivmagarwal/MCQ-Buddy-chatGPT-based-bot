79b442d36e4c796353e1fd290512b1f32efe203d
vivmagarwal <vivmagarwal@gmail.com>
2023-03-29 13:17:38 +0530

    Added proxy server.

--- src/App.js
+++ src/App.js
@@ -23,19 +23,28 @@ const Chatbot = () => {
     setError(null);
 
     try {
+
       const response = await axios.post(
-        "https://api.openai.com/v1/chat/completions",
+        `${process.env.REACT_APP_PROXY_URL}/openai-chat`,
         {
           model: "gpt-3.5-turbo",
           messages: newChatHistory,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
-          },
         }
-      );
+      );      
+
+      // const response = await axios.post(
+      //   "https://api.openai.com/v1/chat/completions",
+      //   {
+      //     model: "gpt-3.5-turbo",
+      //     messages: newChatHistory,
+      //   },
+      //   {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //       Authorization: `Bearer ${process.env.REACT_APP_OPENAI_SECRET}`,
+      //     },
+      //   }
+      // );
 
       console.log(response);
       setLoading(false);
